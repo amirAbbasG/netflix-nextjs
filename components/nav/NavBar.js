decac9e44ec90d3e1d780ff8c72b397e79fca9e8
amir <96867396+etkala@users.noreply.github.com>
2022-03-05 16:37:22 +0330

    initial baner and navbar

---
+++ components/nav/NavBar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Image from "next/image";
+
+import styles from "./navbar.module.css";
+
+const Navbar = ({ username }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink} href="/">
+          <div className={styles.logoWrapper}>
+            <Image
+              src="/static/netflix.svg"
+              alt="Netflix logo"
+              width="128px"
+              height="34px"
+            />
+          </div>
+        </a>
+
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem2}>My List</li>
+        </ul>
+        <nav className={styles.navContainer}>
+          <div>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username}>{username}</p>
+              {/** Expand more icon */}
+              <Image
+                src={"/static/expand_more.svg"}
+                alt="Expand dropdown"
+                width="24px"
+                height="24px"
+              />
+            </button>
+
+            <div className={styles.navDropdown}>
+              <div>
+                <a className={styles.linkName}>Sign out</a>
+                <div className={styles.lineWrapper}></div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
