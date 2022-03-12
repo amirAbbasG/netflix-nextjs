e1e4ce311d86d0e2492c67216e3bae8e7ccd0eac
amir <96867396+etkala@users.noreply.github.com>
2022-03-12 13:17:16 +0330

    test middleware2

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,29 +4,20 @@ import jwt from "@tsndr/cloudflare-worker-jwt";
 
 export async function middleware(req) {
   const token = req ? req.cookies?.token : null;
-  console.log(token);
-  const isValid = await jwt.verify(token, process.env.JWT_SECRET);
-  // Check for validity
-  if (!isValid) {
-    const decodedToken = jwt.decode(token);
-    const userId = decodedToken.issuer;
+  const decodedToken = jwt.decode(token.toString());
+  const userId = decodedToken.issuer;
 
-    const { pathname } = req.nextUrl;
+  const { pathname } = req.nextUrl;
 
-    if (
-      pathname.includes("/api/login") ||
-      userId ||
-      pathname.includes("/static")
-    ) {
-      return NextResponse.next();
-    }
+  if (
+    pathname.includes("/api/login") ||
+    userId ||
+    pathname.includes("/static")
+  ) {
+    return NextResponse.next();
+  }
 
-    if (!token && pathname !== "/login") {
-      const url = req.nextUrl.clone();
-      url.pathname = "/login";
-      return NextResponse.rewrite(url);
-    }
-  } else {
+  if (!token && pathname !== "/login") {
     const url = req.nextUrl.clone();
     url.pathname = "/login";
     return NextResponse.rewrite(url);
