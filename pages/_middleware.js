03f567ac069dc8abca1895ca9b9c4aabe4f5905a
amir <96867396+etkala@users.noreply.github.com>
2022-03-12 12:38:08 +0330

    fix middleware bug

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,22 +1,31 @@
 import { NextResponse } from "next/server";
 
-import { verifyToken } from "../lib/utils";
+import jwt from "@tsndr/cloudflare-worker-jwt";
 
 export async function middleware(req) {
   const token = req ? req.cookies?.token : null;
-  const userId = verifyToken(token);
+  const isValid = await jwt.verify(token, process.env.JWT_SECRET);
+  // Check for validity
+  if (!isValid) {
+    const decodedToken = jwt.decode(token);
+    const userId = decodedToken.issuer;
 
-  const { pathname } = req.nextUrl;
+    const { pathname } = req.nextUrl;
 
-  if (
-    pathname.includes("/api/login") ||
-    userId ||
-    pathname.includes("/static")
-  ) {
-    return NextResponse.next();
-  }
+    if (
+      pathname.includes("/api/login") ||
+      userId ||
+      pathname.includes("/static")
+    ) {
+      return NextResponse.next();
+    }
 
-  if (!token && pathname !== "/login") {
+    if (!token && pathname !== "/login") {
+      const url = req.nextUrl.clone();
+      url.pathname = "/login";
+      return NextResponse.rewrite(url);
+    }
+  } else {
     const url = req.nextUrl.clone();
     url.pathname = "/login";
     return NextResponse.rewrite(url);
