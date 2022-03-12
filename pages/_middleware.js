199ff21a8230d92926b0fc1c6d168c3b1d036499
amir <96867396+etkala@users.noreply.github.com>
2022-03-12 11:10:01 +0330

    fix bug2

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 import { verifyToken } from "../lib/utils";
 
-export async function middleware(req, ev) {
+export async function middleware(req) {
   const token = req ? req.cookies?.token : null;
   const userId = verifyToken(token);
 
