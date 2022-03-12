b61654885d66efbe877aab42c8c434fcbb5d5ab5
amir <96867396+etkala@users.noreply.github.com>
2022-03-12 14:38:25 +0330

    change middleware cookie

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,10 +4,8 @@ import { NextResponse } from "next/server";
 import jwt from "@tsndr/cloudflare-worker-jwt";
 
 export async function middleware(req) {
-  console.log({ req });
   const token = req.cookies?.token ? req.cookies?.token : null;
-  console.log({ cookie: req.cookies });
-  console.log({ token });
+
   // const userId = await verifyToken(token);
   const decodedToken = jwt.decode(token);
   const userId = decodedToken.issuer;
